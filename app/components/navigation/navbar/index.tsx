652117331ea1bba29df8a6b656ccdc02364d7e49
pocolops <129567423+Pocolops@users.noreply.github.com>
2023-08-30 21:41:23 +0800

    center

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -6,7 +6,7 @@ const authUserName = 'john'
 
 function NavItems() {
   return (
-    <div className='items-center justify-between w-full md:flex md:w-auto' id="nav-items">
+    <div className='items-center justify-between w-full md:flex md:w-auto h-20' id="nav-items">
       <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white'>
         {
           navOptions.map((item) => (
