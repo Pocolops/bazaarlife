a3fe7b0bbbb9de0e992a43e04ac5295a246c95b7
pocolops <129567423+Pocolops@users.noreply.github.com>
2023-08-30 21:31:00 +0800

    navbar

---
+++ app/components/navigation/common.ts
@@ -0,0 +1,28 @@
+export const navOptions = [
+    {
+        id: "Listing",
+        label: "All products",
+        path: "/products/listing/all-products",
+    },
+    {
+        id: "listingElectronics",
+        label: "Electronics",
+        path: "/products/listing/electronics",
+    },
+    {
+        id: "listingFashion",
+        label: "Fashion",
+        path: "/products/listing/fashion",
+    },
+    {
+        id: "listingSports",
+        label: "Sports",
+        path: "/products/listing/sports",
+    },
+    {
+        id: "listingServices",
+        label: "Services",
+        path: "/products/listing/services",
+    },
+    
+];
--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -1,38 +1,58 @@
-import React from "react";
-import Link from "next/link";
-import Logo from "./Logo";
-import Button from "./Button";
+import React, { Fragment } from 'react';
+import {navOptions} from '../common';
+
+const authUser = true;
+const authUserName = 'john'
+
+function NavItems() {
+  return (
+    <div className='items-center justify-between w-full md:flex md:w-auto' id="nav-items">
+      <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white'>
+        {authUser &&
+          navOptions.map((item) => (
+            <li className='cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0' key={item.id}>
+              {item.label}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+
+
 
 const Navbar = () => {
   return (
-    <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <Logo />
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/about">
-                  <p>About Us</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li>
-            </ul>
-            <Button />
-          </div>
+    <nav className='bg-white fixed w-full z-20 top-0 left-0 border-b h-20 border-gray-200'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto'>
+        <div className='flex items-center cursor pointer'>
+          <span className='self-center text-2xl font-semibold whitespace-nowrap'>Bazaar Life</span>
         </div>
+        <div className='flex md:order-2 gap-2' >
+          {authUser ?(
+             <Fragment>
+               <div className='flex items-center gap-2'>
+                <div className='w-8 h-8 rounded-full bg-gray-300'>
+                  {/* Insert your avatar image here */}
+                </div>
+                <span>"Hello, {authUserName}"</span>
+              </div>
+             <button>Account</button>
+             <button>Cart</button>
+             <button>Sell</button>
+           </Fragment>
+          ): null}
+          {
+            authUser ? <button>Logout</button> : <button>Login</button>
+          }
+         
+        </div>
+        <NavItems/>
       </div>
-    </>
-  );
-};
 
-export default Navbar;
+    </nav>
+  )
+}
+
+export default Navbar
