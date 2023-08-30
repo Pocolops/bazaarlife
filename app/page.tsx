33e8b54bc85e611639941e54f5f212dd1e44ea59
Aiden <aidenchenyx@outlook.com>
2023-08-30 18:14:00 +0800

    a

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>BazaarLifeeee</h1>
       <h2>hi</h2>
-      
+      <h3><b>
+  "Discover, Shop, and Connect – Your Ultimate Ecommerce Experience with BazaarLive!"</b></h3>
     </main>
   )
 }
