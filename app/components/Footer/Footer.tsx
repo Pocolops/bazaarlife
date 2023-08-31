79a4d22168dce93d000a49ab8aed588c7174bb82
yazhernandez <yazihernandez104@gmail.com>
2023-08-31 23:40:31 +0800

    debug4

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import logo from '../../../public/assets/logo/favicon.png';
+import logo from '../../../public/assets/images/default.png';
 
 const Footer = () => {
   return (
