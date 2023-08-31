16afda1d08100f300e945d3832b36d2d820cea2c
pocolops <129567423+Pocolops@users.noreply.github.com>
2023-08-31 21:41:06 +0800

    ea

--- app/components/Hero/hero.tsx
+++ app/components/Hero/hero.tsx
@@ -1,27 +1,31 @@
-"use client"
+'use client'
 import React from 'react';
-
 import { Carousel } from 'react-responsive-carousel';
+
+import SliderImg_1 from '../../../public/images/slider/sliderImg_1.jpg';
+import SliderImg_2 from '../../../public/images/slider/sliderImg_2.jpg'
+import SliderImg_3 from '../../../public/images/slider/sliderImg_3.jpg';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
     <div>
-        <Carousel>
-                <div>
-                    <img src="" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="" />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
+      <Carousel>
+        <div>
+          <Image src={SliderImg_1} alt="Slider 1" />
+          
+        </div>
+        <div>
+          <Image src={SliderImg_2} alt="Slider 2" />
+         
+        </div>
+        <div>
+          <Image src={SliderImg_3} alt="Slider 3" />
+          
+        </div>
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
