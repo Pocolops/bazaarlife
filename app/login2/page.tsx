2c9eed574c55c2722b5783851cf0cdd1328c61cc
Aiden <aidenchenyx@outlook.com>
2023-08-31 23:53:14 +0800

    registration

---
+++ app/login2/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+
+export default function Login() {
+
+    return (
+        <div className="bg-white relative" style={{color:'black'}}>Log In</div>
+    )
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,18 @@
 
-
+'use client';
 
 
 export default function Register() {
 
     return (
-        <div className="bg-white relative" style={{color:'black'}}>Register</div>
+        <div className="bg-white relative" style={{color:'black'}}>
+            <div className="flex flex-col item-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto max-w-7xl xl:px-5 lg:flex-row">
+                <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row">
+                    <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
