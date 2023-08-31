2274fca0718b77e16aea00786febb131dbd3ea1a
pocolops <129567423+Pocolops@users.noreply.github.com>
2023-08-31 17:53:31 +0800

    ea

--- app/components/Context/index.tsx
+++ app/components/Context/index.tsx
@@ -1,24 +1,19 @@
-"use client"
-import React, { createContext, useState, ReactNode } from "react";
+'use client'
+import React, { createContext, useState, ReactNode, useContext } from "react";
 
 interface GlobalStateContextType {
-  showNavModal: boolean;
-  setShowNavModal: React.Dispatch<React.SetStateAction<boolean>>;
+    showNavModal: boolean;
+    setShowNavModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const GlobalContext = createContext<GlobalStateContextType | null>(null);
 
-interface GlobalStateProps {
-  children: ReactNode;
+export default function GlobalState({ children }: { children: ReactNode }) {
+    const [showNavModal, setShowNavModal] = useState(false);
+
+    return (
+        <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>
+            {children}
+        </GlobalContext.Provider>
+    );
 }
-
-export default function GlobalState({ children }: GlobalStateProps) {
-  const [showNavModal, setShowNavModal] = useState(false);
-
-  return (
-    <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>
-      {children}
-    </GlobalContext.Provider>
-  );
-}
-
