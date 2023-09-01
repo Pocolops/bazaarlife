e5a8a5a554addbcb94ab74454c1ca3eeb05e4dcf
Aiden <aidenchenyx@outlook.com>
2023-09-02 06:37:16 +0800

    updated login2 and registration page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -40,6 +40,12 @@ export default function Register() {
                       />
                     ) : null
                   )}
+                  <button
+                  className='inline-flex w-full items-center justify-center bg-[#A6E3E9] px-6 py-4 text-lg
+                  text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide'
+                  >
+                    Register
+                  </button>
                 </div>
               )}
             </div>
