afbfd0c58d7cbf682010cda9a551cbe751d17d36
Aiden <aidenchenyx@outlook.com>
2023-09-02 21:21:55 +0800

    added user.js schema&downloaded bcrypt dependency

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from 'next-auth/providers/credentials';
 import connectToDB from "../../../database/database"
-import Users from '../../../model/Schema'
+import User from '../../../models/user'
 import { compare } from 'bcryptjs';
 
 export default NextAuth({
